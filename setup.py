93c6a78b660dc7e3cd85d6449003f9498128c6fc
Sander Schaminee <8972335+GeoSander@users.noreply.github.com>
2019-10-07 15:52:17 +0200

    Removed setup.cfg and changed setup.py (in accordance with Python 2.7 gpf package)

--- setup.py
+++ setup.py
@@ -14,29 +14,47 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 from setuptools import setup, find_packages
 
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+tests_require = ['pytest', 'pytest-cov', 'mock', 'pytest-mock']
 setup(
         name='gpf3',
-        packages=find_packages(exclude=('tests',)),
-        version='0.1',
+        packages=find_packages(exclude=('tests', 'docs')),
+        use_scm_version=True,
+        setup_requires=['setuptools_scm'],
         license='Apache License 2.0',
-        description='Scripting framework for ArcPy (ArcGIS Pro 2.2+).',
+        description='Geocom Python Framework for ArcPy (Esri ArcGIS Pro).',
+        long_description_content_type='text/x-rst',
+        long_description=read('README.rst'),
         author='Geocom Informatik AG / VertiGIS, Burgdorf, Switzerland',
         author_email='github@geocom.ch',
         url='https://github.com/geocom-gis/gpf3',
-        # download_url='https://github.com/geocom-gis/gpf3/archive/gpf3_v010.tar.gz',
+        project_urls={
+            'Source': 'https://github.com/geocom-gis/gpf3',
+            'Documentation': 'https://gpf3.readthedocs.io/'
+        },
         keywords=[
             'Geocom', 'GIS', 'GEONIS', 'tools', 'scripting', 'framework', 'spatial',
             'geospatial', 'geoprocessing', 'Esri', 'ArcGIS', 'ArcPy', 'VertiGIS'
         ],
+        python_requires='>=3.6',
+        tests_require=tests_require,
+        extras_require={
+            'test': tests_require
+        },
         classifiers=[
-            'Development Status :: 3 - Alpha',  # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+            'Development Status :: 4 - Beta',  # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
             'Intended Audience :: Developers',
             'Environment :: Other Environment',
             'Operating System :: Microsoft :: Windows',
             'Topic :: Scientific/Engineering :: GIS',
-            'License :: OSI Approved :: Apache License 2.0',
-            'Programming Language :: Python :: 3.6',
+            'License :: OSI Approved :: Apache Software License',
+            'Programming Language :: Python :: 3.6'
         ]
 )
