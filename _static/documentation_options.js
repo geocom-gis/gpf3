68e9047f5e8b6a45d10521e1921915d344f0fcc0
Sander Schaminee <sander.schaminee@geocom.ch>
2020-02-03 23:38:14 +0100

    Updated Sphinx HTML build

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev8+gd123c1c.d20191007',
+    VERSION: '0.4.dev27+ga276b54.d20200203',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
